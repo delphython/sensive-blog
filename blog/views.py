1ba30d73e99ea89056fe997eb9bc32d26f3087c8
delphython <delphython@gmail.com>
2021-10-18 11:25:15 +0300

    Add prefetch_related to post

--- blog/views.py
+++ blog/views.py
@@ -57,7 +57,7 @@ def index(request):
 
 def post_detail(request, slug):
     try:
-        post = Post.objects.get(slug=slug).prefetch_related("author")
+        post = Post.objects.prefetch_related("author").get(slug=slug)
     except Post.DoesNotExist:
         raise Http404("Post does not exist")
 
